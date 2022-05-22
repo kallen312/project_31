d58dcf510a0c5794cbb40a15224aedc9831ea2c2
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-05-22 13:32:39 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const cards = [
+    "orange",
+    "blue",
+    "pink",
+    "white",
+    "orange",
+    "blue",
+    "pink",
+    "white"
+  ];
+
+  return <div className="App"></div>;
+}
+
+function Card(props) {
+  return;
+}
