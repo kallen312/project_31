24710c8aeb705f89879bca2e3d77533763062034
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-06-01 18:32:55 -0400

    left off on comparing the two cards

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ numbers = [0,1,2,3]
 something.map(() => {
   return ....
 })
+
+counter to know if it's a first click or second click
+variable for click 1
+variable for click 2
+if click1 !== click2 flip the last two cards over to black
+if click1 === click2 then the user can keep click more cards
 */
 
+// use this component to keep track of multiple cards
 export default function App() {
   const cards = [
     "orange",
@@ -22,7 +29,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <div>
+      <div className="grid">
         {cards.map((color) => {
           return <Card color={color} />;
         })}
@@ -31,14 +38,40 @@ export default function App() {
   );
 }
 
+// this component deals with a single card
 function Card(props) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const [card1, setCard1] = useState("");
+
+  const [card2, setCard2] = useState("");
+
+  const [counter, setCounter] = useState(0);
+  // if it isFlipped = true, then show the current card
+  // if not, show a card with a black background
+
+  // write a function
+  // add condition
+  // return '' or props.color
+
+  function handleClick() {
+    setIsFlipped(true);
+
+    setCard1(props.color);
+    //setCounter(counter + 1)
+  }
+
+  function changeStyle() {
+    if (isFlipped) {
+      return { backgroundColor: props.color };
+    } else {
+      return { backgroundColor: "black" };
+    }
+  }
   return (
-    <div class="grid" style={{ backgroundColor: props.color }}>
+    // you can use the isFlipped within the style as a ternary
+    <div className="cardStyle" style={changeStyle()} onClick={handleClick}>
       {props.color}
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
     </div>
   );
 }
